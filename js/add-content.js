a07fb3001ac93c2c4b5543f315fa6d6425c7b7d6
israaothman <israaayoub@hotmail.com>
2020-02-04 15:46:05 +0200

    add js

---
+++ js/add-content.js
@@ -0,0 +1,12 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting; 
+if (hourNow > 18){ 
+    greeting ='Goodevening, Class!';}
+    else if (hourNow > 12) {
+        greeting='Goodafternoon, Class!';}
+    else if(hourNow >= 0){
+            greeting='Goodmorning!';}
+    else{greeting ='Somethingwentwrong!';}
+            
+ document.write('<h3>'+greeting+'</h3>');
